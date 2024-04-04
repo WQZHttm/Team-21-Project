73b005d820fe38017327621a3aa2b7f261cfe9a4
matchasoftserve <tanshcheryl12@gmail.com>
2024-04-04 16:02:51 +0800

    sidebar draft 1

--- main.py
+++ main.py
@@ -1,40 +1,69 @@
-from dash import Dash, html, dcc
 import dash
-import plotly.express as px
-import datetime
 import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+from pages import day, week
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# TO MOVE TO STYLES.CSS
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.Img(src='https://www.mountfaberleisure.com/wp-content/uploads/2023/08/logo.png'),
+        html.Br(),
+        html.Br(),
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Daily", href="/", active="exact"),
+                dbc.NavLink("Week", href="/week", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return day.layout
+    elif pathname == "/week":
+        return week.layout
+    # If the user tries to reach a different page, return a 404 message
+    return html.Div(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ],
+        className="p-3 bg-light rounded-3",
+    )
 
-#setting the date
-current_date = datetime.datetime.now()
-start_of_current_week = current_date - datetime.timedelta(days=current_date.weekday())
-end_of_current_week = start_of_current_week + datetime.timedelta(days=6)
-start_date_default = start_of_current_week.date()
-end_date_default = end_of_current_week.date()
 
-# LAYOUT
-px.defaults.template = "ggplot2"
-
-external_css = ["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
-
-app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
-
-app.layout = html.Div([
-	html.Br(),
-    html.Img(src='https://www.mountfaberleisure.com/wp-content/uploads/2023/08/logo.png', style={'height': '50px', 'margin-right': '10px','float': 'left'}),
-	html.Br(),
-	html.Br(),	
-	html.H1(datetime.datetime.now().strftime('Last updated: %Y-%m-%d %H:%M:%S'),
-		style={'opacity': '1','color': 'blue', 'fontSize': 15, 'position': 'absolute', 'top': '2px', 'right': '2px'}),
-    html.Br(),
-    html.Div(children=[
-	    dcc.Link(page['name'], href=page["relative_path"], className="btn btn-dark m-2 fs-5")\
-			  for page in dash.page_registry.values()], style={'height': '50px', 'margin-right': '10px'}
-	),
-    html.Br(),
-	dash.page_container
-], className="col-8 mx-auto")
-
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run_server()
