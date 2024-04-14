3225ddf04c04dc355e089a065b40275f1b06fd44
Warren <voidtheo1998@gmail.com>
2024-04-14 18:14:13 +0800

    added more stuff

--- code/backend_main.py
+++ code/backend_main.py
@@ -1,9 +1,12 @@
 # import sys
 # sys.path.append('.')
-#from schedule import *
+from schedule import *
 from decision_tree_model import *
 
 df = pd.read_csv('../input/data_with_hour.csv')
 data2024 = pd.read_csv('../input/test.csv')
 
-# stuff = DecisionTree()
+stuff = DecisionTree(df, data2024)
+#Finally = smart_Schedule(stuff)
+
+### Finally is a dataframe already in the desired format, that needs to be sent to database
