673dfc58fca0129a1878250421f349e5bd7e7817
matchasoftserve <tanshcheryl12@gmail.com>
2024-04-24 09:51:21 +0800

    cleaned up junk

--- main.py
+++ main.py
@@ -1,42 +1,83 @@
 from dash import Dash, html, dcc,Input, Output,callback
 import dash
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.types import TypeDecorator, VARCHAR
+from dateutil.parser import parse
 import plotly.express as px
 import datetime
 import dash_bootstrap_components as dbc
 import pandas as pd
-import mysql.connector
-import yaml
 
-####################### SQL INTEGRATION #############################
-# with open("docker-compose.yml", "r") as file:
-#     config =yaml.safe_load(file)['services']['db']['environment']
-    
+# import mysql.connector
+# import yaml
 
-# def fetch_data(table):
+####################### FLASK SQL #############################
 
-#     cnx = mysql.connector.connect(
-#         host='db',
-#         user=config['MYSQL_USER'],
-#         password=config['MYSQL_PASSWORD'],
-#         database=config['MYSQL_DATABASE']
-#     )
-#     cursor = cnx.cursor()
-#     print('connected', cnx.is_connected())
-#     cursor.execute(f"SELECT * FROM {table}")
-#     result = cursor.fetchall()
-#     cursor.close()
-#     cnx.close()
-#     return pd.DataFrame(result)
+# server = Flask(__name__)
 
 
+# @server.route("/flask")
+# def home():
+#     return "Flask Database"
+# server.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://your_db_user:your_db_password@db/your_db_name'
+# server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# db = SQLAlchemy(server)
 
 
+# class CustomDate(TypeDecorator):
+#     impl = VARCHAR
+
+#     def process_bind_param(self, value, dialect):
+#         if isinstance(value, str):
+#             # Assuming input string is in format 'dd/mm/yyyy'
+#             return value
+#         elif isinstance(value, datetime):
+#             return value.strftime('%d/%m/%Y')
+#         else:
+#             raise ValueError('Invalid input format for date')
+
+#     def process_result_value(self, value, dialect):
+#         if value is not None:
+#             return parse(value).date()
+#         return value
+
+
+# class Schedule(db.Model):
+#     id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+#     Date=db.Column(CustomDate,nullable=False)
+#     Day=db.Column(db.String(20),nullable=False)
+#     Public_Holiday=db.Column(db.String(120))
+#     Employee_ID=db.Column(db.String(5),nullable=False)
+#     Shift=db.Column(db.String(120),nullable=False)
+#     Role=db.Column(db.String(120))
+#     Hours_worked=db.Column(db.Integer)
+#     Hourly_rate=db.Column(db.Integer)
+#     Job_status=db.Column(db.String(120))
+#     # posts=db.relationship('Post',backref='author',lazy=True) #UPDATE NULLABLE
+
+#     # MAY NOT NEED
+#     def __repr__(self) -> str:
+#         return f"<Schedule(id='{self.id}', Date='{self.Date}', Day='{self.Day}', Public_Holiday='{self.Public_Holiday}', Employee_ID='{self.Employee_ID}', Shift='{self.Shift}', Role='{self.Role}', Hours_worked='{self.Hours_worked}', Hourly_rate='{self.Hourly_rate}', Job_status='{self.Job_status}')>"
+
+
+# import csv
+# # db.drop_all() # drop tables
+# db.create_all()
+# with open("final.csv") as f:
+#     reader = csv.reader(f)
+#     header = next(reader)
+#     for i in reader:
+#         kwargs = {column: value for column, value in zip(header, i)}
+#         new_entry = Schedule(**kwargs)
+#         db.session.add(new_entry)
+#         db.session.commit()
 
 ####################### DASH APP #############################
 
 external_css = [dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP,dbc.icons.FONT_AWESOME,"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
 
-app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css)
+app = Dash(__name__, pages_folder='pages', use_pages=True, external_stylesheets=external_css,routes_pathname_prefix="/")
 # auth=dash_auth.BasicAuth(app,USER_PASS_MAPPING)
 
 sidebar = html.Div([
@@ -90,4 +131,10 @@ app.layout = html.Div([
 
 
 if __name__ == '__main__':
+    # with server.app_context():
+    #     db.create_all()
+    #     schedule=Schedule()
+    #     db.session.add(schedule)
+    #     db.session.commit()
+    #     print(Schedule.query.all())
 	app.run(debug=True)
